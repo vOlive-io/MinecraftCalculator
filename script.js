464effffca604df6da24b8ff5a0f4683874ab818
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-01-04 17:26:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var items = [];
 var defultItems = [
     ["Dirt", false, null, "dirt.png"], 
     ["Stone", false, null, "stone.png"], 
-    ["Oak Logs", false, null, "wood.png"]];
+    ["Oak Logs", false, null, "wood.png"]
+]
+var recipes = [];
+
+function addRecipe() {
+
+}
