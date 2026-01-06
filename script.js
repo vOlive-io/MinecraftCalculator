9d1111070b8f71b5313dabec0ab427271cd4540b
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-01-06 16:40:18 -0700

    Made test script

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+async function testLoad() {
+  const items = await fetch("./data/pc/1.21.1/items.json").then(r => r.json());
+  const recipes = await fetch("./data/pc/1.21.1/recipes.json").then(r => r.json());
+
+  console.log("Items loaded:", items.length);
+  console.log("Recipes keys:", Object.keys(recipes).length);
+}
+
+testLoad();
+
+
+
 var items = [];    
 var defultItems = [
     ["Dirt", false, null, "dirt.png"], 
