3f124aef2a18ecbe7983839dcee8798c133f759a
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2026-01-06 17:57:52 -0700

    Removed test cases

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-async function testLoad() {
-  const items = await fetch("./data/pc/1.21.1/items.json").then(r => r.json());
-  const recipes = await fetch("./data/pc/1.21.1/recipes.json").then(r => r.json());
+var items = await fetch("./data/pc/1.21.1/items.json").then(r => r.json());
+var recipes = await fetch("./data/pc/1.21.1/recipes.json").then(r => r.json());
 
-  console.log("Items loaded:", items.length);
-  console.log("Recipes keys:", Object.keys(recipes).length);
-}
-
-
-
-
-
-var items = [];    
-var defultItems = [
-    ["Dirt", false, null, "dirt.png"], 
-    ["Stone", false, null, "stone.png"], 
-    ["Oak Logs", false, null, "wood.png"]
-]
-var recipes = [];
-
-function addRecipe() {
-
-}
-
-testLoad();
+console.log("Items loaded:", items.length);
+console.log("Recipes keys:", Object.keys(recipes).length);
